4db7e297faf4abccaa8af74ab1e94f3cde60e5a0
Gábor Szabó <szabo.gabor@podnet.hu>
2023-07-04 16:19:47 +0200

    Removed leftover root component registrations from client side

--- MudJSInteropExceptionHandlingHostedWasm/Client/Program.cs
+++ MudJSInteropExceptionHandlingHostedWasm/Client/Program.cs
@@ -4,8 +4,6 @@ using MudBlazor.Services;
 using MudJSInteropExceptionHandlingHostedWasm.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
